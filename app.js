3324f7389fa96be84750c9c75e9ddb465d17e4bb
Sksham Kaushal <skshamkaushal@gmail.com>
2025-06-04 15:11:32 +0530

    op

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const app = express(); //Initialized and server ready
 
 app.use(express.static("public"));
 
-let port = process.env.PORT || 6969;
+let port = process.env.PORT || 5000;
 let server = app.listen(port, () => {
-     console.log(`Port is running ta http://localhost:${port}`);
-    
+    console.log("Listening to port" + port);
 })
 
 let io = socket(server);
